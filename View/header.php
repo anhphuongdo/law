41938f7bb159c3502e990f6a16dc7ac5cb3741b6
anhphuongdo <anhdo.31201024387@st.ueh.edu.vn>
2023-04-13 16:08:39 +0700

    new repo

---
+++ View/header.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <?php
+            echo "This is header";
+        ?>
+    </body>
+</html>
---
+++ View/index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>TỔNG HỢP NGHIÊN CỨU KHOA HỌC SINH VIÊN</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../style/nckh.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Itim|Lobster|Montserrat:500|Noto+Serif|Nunito|Patrick+Hand|Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i|Roboto+Slab|Saira" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                include $_SERVER['DOCUMENT_ROOT']."/1.law/View/header.php"
+            ?>
+            <p>Hello World</p>
+        </div>
+    </body>
+    <footer>
+        <?php
+        include $_SERVER['DOCUMENT_ROOT']."/1.law/View/footer.php"
+        ?>
+    </footer>
+</html>
